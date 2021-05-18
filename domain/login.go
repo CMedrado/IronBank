d67eec166fe400f061077db1cf02a64fde889771
CMedrado <rafaelcastromedrado@gmail.com>
2021-05-18 19:15:55 -0300

    Feat: Turns the token into base64

--- domain/login.go
+++ domain/login.go
@@ -1,29 +1,33 @@
 package domain
 
 import (
+	"encoding/base64"
 	"github.com/CMedrado/DesafioStone/store"
+	"strconv"
 )
 
-func (auc AccountUsecase) AuthenticatedLogin(cpf, secret string) (error, int) {
+func (auc AccountUsecase) AuthenticatedLogin(cpf, secret string) (error, string) {
 	secretHash := Hash(secret)
 
 	err := CheckedError(cpf)
+	cpf = CpfReplace(cpf)
 	if err != nil {
-		return err, 0
+		return err, ""
 	}
 
-	cpf = CpfReplace(cpf)
 	newLogin := store.Login{cpf, secretHash}
 	account := auc.Store.CheckLogin(cpf)
 
 	err = CheckLogin(account, newLogin)
 	if err != nil {
-		return err, 0
+		return err, ""
 	}
 
-	token := Random()
 	id := auc.Store.TransferredAccounts()
-	auc.Token.CreatedToken(id[cpf].ID, token)
+	now := CreatedAt()
+	token := now + ":" + strconv.Itoa(id[cpf].ID)
+	encoded := base64.StdEncoding.EncodeToString([]byte(token))
+	auc.Token.CreatedToken(id[cpf].ID, encoded)
 
-	return nil, token
+	return nil, encoded
 }

cf74f2a5b979fc5a6e0c493fed1078089421cfaf
CMedrado <rafaelcastromedrado@gmail.com>
2021-06-10 11:05:17 -0300

    feat:implement repository

--- domain/account/repository.go
+++ domain/account/repository.go
@@ -3,7 +3,9 @@ package account
 import "github.com/CMedrado/DesafioStone/store"
 
 type Repository interface {
-	GetAccounts() []store.Account
-	GetBalance(int) (int, error)
-	CreateAccount(string, string, string, int) (int, error)
+	GetAccountCPF(string) store.Account
+	CreateAccount(account store.Account) (int, error)
+	GetAccounts() map[string]store.Account
+	UpdateBalance(person1, person2 store.Account)
+	ReturnCPF(cpf string) int
 }
--- domain/login/repository.go
+++ domain/login/repository.go
@@ -1,5 +1,9 @@
 package login
 
+import "github.com/CMedrado/DesafioStone/store"
+
 type Repository interface {
-	AuthenticatedLogin(string, string) (error, string)
+	ReturnToken(id int) string
+	PostToken(id int, token string)
+	GetTokenID(id int) store.Token
 }

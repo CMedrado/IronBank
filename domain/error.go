60352fe00c9b7086200bdf3f1f03d189ad8ff007
CMedrado <rafaelcastromedrado@gmail.com>
2021-06-18 18:40:41 -0300

    fix: replace person1 that was struct with an int

--- domain/error.go
+++ domain/error.go
@@ -36,8 +36,8 @@ func CheckCPF(cpf string) error {
 }
 
 // CheckAccountBalance checks if the account has a balance and returns nil if not, it returns an error
-func CheckAccountBalance(person1 store.Account, amount int) error {
-	if person1.Balance < amount {
+func CheckAccountBalance(person1 int, amount int) error {
+	if person1 < amount {
 		return errWithoutBalance
 	}
 	return nil

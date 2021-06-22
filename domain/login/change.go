24c4e051a2a94dea78fc34caf52a586a834db322
CMedrado <rafaelcastromedrado@gmail.com>
2021-06-22 15:43:14 -0300

    feat: create function to convert from account entity from storage to account domain or vice versa

---
+++ domain/login/change.go
@@ -0,0 +1,16 @@
+package login
+
+import (
+	"github.com/CMedrado/DesafioStone/domain"
+	"github.com/CMedrado/DesafioStone/storage/file/token"
+)
+
+//func ChangeTokenDomain(tokenDomain domain.Token) token.Token {
+//	tokenStorage := token.Token{ID: tokenDomain.ID,Token: tokenDomain.Token}
+//	return tokenStorage
+//}
+
+func ChangeTokenStorage(tokenStorage token.Token) domain.Token {
+	tokenDomain := domain.Token{ID: tokenStorage.ID, Token: tokenStorage.Token}
+	return tokenDomain
+}

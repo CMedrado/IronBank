e559469690f97e96658a842b472585cdae8fcd65
CMedrado <rafaelcastromedrado@gmail.com>
2021-06-22 15:54:13 -0300

    style: separate get_post from token into 2 files and change names

--- storage/file/token/save_token.go
+++ storage/file/token/save_token.go
@@ -1,14 +1,12 @@
 package token
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
-func (a *StoredToken) PostToken(id int, token string) {
+func (a *StoredToken) SaveToken(id int, token string) {
 	a.tokens = append(a.tokens, Token{ID: id, Token: token})
 	a.dataBase.Seek(0, 0)
 
 	json.NewEncoder(a.dataBase).Encode(a.tokens)
-}
-
-func (a *StoredToken) GetTokens() []Token {
-	return a.tokens
 }

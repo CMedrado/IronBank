cc2f011d204d3893947a7d935dac08bb7185da5f
CMedrado <rafaelcastromedrado@gmail.com>
2021-06-22 00:07:15 -0300

    style: remove what is not necessary

--- store/login/store.go
+++ store/login/store.go
@@ -1,16 +1,6 @@
 package login
 
-var accountLogin = make(map[int]Login)
-
 type Login struct {
 	CPF    string `json:"cpf"`
 	Secret string `json:"secret"`
-}
-
-type StoredLogin struct {
-	accountLogin map[int]Login
-}
-
-func NewStoredLogin() *StoredLogin {
-	return &StoredLogin{accountLogin: accountLogin}
 }

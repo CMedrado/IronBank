b26ecc7f2d17e858925d1d00e548f62e7d739b53
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2021-06-29 19:09:18 -0300

    Revert "style: remove what is not necessary"
    This reverts commit 7f44ec7a8a8c323124f2c545ec01dd0c0c8b13d2.

--- store/login/store.go
+++ store/login/store.go
@@ -1,6 +1,16 @@
 package login
 
+var accountLogin = make(map[int]Login)
+
 type Login struct {
 	CPF    string `json:"cpf"`
 	Secret string `json:"secret"`
+}
+
+type StoredLogin struct {
+	accountLogin map[int]Login
+}
+
+func NewStoredLogin() *StoredLogin {
+	return &StoredLogin{accountLogin: accountLogin}
 }

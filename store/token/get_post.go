fd0c67225b94728a74d449d494ad29506efea2fd
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2021-06-29 19:09:18 -0300

    Revert "style: separate get_post from token into 2 files and change names"
    This reverts commit cdfc9b3d896dcc2c1c10aa1c2909df6b8ac75e00.

--- store/token/get_post.go
+++ store/token/get_post.go
@@ -1,12 +1,14 @@
 package token
 
-import (
-	"encoding/json"
-)
+import "encoding/json"
 
-func (a *StoredToken) SaveToken(id int, token string) {
+func (a *StoredToken) PostToken(id int, token string) {
 	a.tokens = append(a.tokens, Token{ID: id, Token: token})
 	a.dataBase.Seek(0, 0)
 
 	json.NewEncoder(a.dataBase).Encode(a.tokens)
+}
+
+func (a *StoredToken) GetTokens() []Token {
+	return a.tokens
 }

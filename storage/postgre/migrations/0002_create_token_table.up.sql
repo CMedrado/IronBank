c011867a531e77fadb887fd7f97e10fe1f2624cc
CMedrado <rafaelcastromedrado@gmail.com>
2021-07-05 18:39:21 -0300

    chore: add migration

---
+++ storage/postgre/migrations/0002_create_token_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tokens(
+    id INTEGER PRIMARY KEY,
+    id_account UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+
+    CONSTRAINT token_owner_account_fk
+        FOREIGN KEY (id_account) REFERENCES accounts
+);

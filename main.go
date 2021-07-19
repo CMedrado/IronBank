892b52b9e7e4bd5f857c8f4e3aa0f35ec9e63c47
rafael <rafaelcastromedrado@gmail.com>
2021-07-19 13:50:26 -0300

    refactor: fix main to sql

--- main.go
+++ main.go
@@ -1,32 +1,78 @@
 package main
 
 import (
+	"context"
 	"github.com/CMedrado/DesafioStone/domain/account"
 	"github.com/CMedrado/DesafioStone/domain/authentication"
 	"github.com/CMedrado/DesafioStone/domain/transfer"
-	https "github.com/CMedrado/DesafioStone/https"
+	"github.com/CMedrado/DesafioStone/https"
 	http_account "github.com/CMedrado/DesafioStone/https/account"
 	http_login "github.com/CMedrado/DesafioStone/https/authentication"
 	http_transfer "github.com/CMedrado/DesafioStone/https/transfer"
-	store_account "github.com/CMedrado/DesafioStone/storage/file/account"
-	store_token "github.com/CMedrado/DesafioStone/storage/file/token"
-	store_transfer "github.com/CMedrado/DesafioStone/storage/file/transfer"
+	"github.com/CMedrado/DesafioStone/storage/postgre"
+	store_account "github.com/CMedrado/DesafioStone/storage/postgre/accounts"
+	store_token "github.com/CMedrado/DesafioStone/storage/postgre/token"
+	store_transfer "github.com/CMedrado/DesafioStone/storage/postgre/transfer"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/sirupsen/logrus"
 	"net/http"
 	"os"
-	time "time"
+	"time"
 )
 
 const dbFileNameAccount = "accounts.db.json"
 const dbFileNameToken = "token.db.json"
 const dbFileNameTransfer = "transfer.db.json"
 const Port = "5000"
+const dbUrl = "postgresql://postgres:example@localhost:5432/desafio?sslmode=disable"
 
 func main() {
 	logger := logrus.New()
 	logger.SetFormatter(&logrus.TextFormatter{TimestampFormat: time.RFC3339})
 	lentry := logrus.NewEntry(logger)
 
+	err := postgre.RunMigrations(dbUrl)
+	if err != nil {
+		lentry.Fatal(os.Stderr, "failed to run migrations: %v\n", err)
+		os.Exit(1)
+	}
+
+	pool, err := getDbPool(dbUrl)
+	if err != nil {
+		lentry.Fatal("failed to get db connection pool: %v", err)
+	}
+
+	accountStorage := store_account.NewStored(pool, lentry)
+	accountToken := store_token.NewStored(pool, lentry)
+	accountTransfer := store_transfer.NewStored(pool, lentry)
+	accountUseCase := account.UseCase{StoredAccount: accountStorage}
+	loginUseCase := authentication.UseCase{AccountUseCase: &accountUseCase, StoredToken: accountToken}
+	transferUseCase := transfer.UseCase{AccountUseCase: &accountUseCase, StoredTransfer: accountTransfer, TokenUseCase: &loginUseCase}
+	accountHandler := http_account.NewHandler(&accountUseCase, lentry)
+	loginHandler := http_login.NewHandler(&loginUseCase, lentry)
+	transferHandler := http_transfer.NewHandler(&transferUseCase, lentry)
+	server := https.NewAPI(accountHandler, loginHandler, transferHandler, lentry)
+
+	lentry.WithField("Port", Port).Info("starting the server!")
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		lentry.Fatal("could not hear on port 5000 ")
+	}
+	lentry.WithField("Port", Port).Info("shutting down the server")
+}
+
+func getDbPool(dburl string) (*pgxpool.Pool, error) {
+	conf, err := pgxpool.ParseConfig(dburl)
+	if err != nil {
+		return nil, err
+	}
+	pool, err := pgxpool.ConnectConfig(context.Background(), conf)
+	if err != nil {
+		return nil, err
+	}
+	return pool, nil
+}
+
+func openFiles(dbFileNameAccount string, dbFileNameToken string, dbFileNameTransfer string, lentry *logrus.Entry) (*os.File, *os.File, *os.File) {
 	dbAccount, err := os.OpenFile(dbFileNameAccount, os.O_RDWR|os.O_CREATE, 0666)
 
 	if err != nil {
@@ -45,19 +91,5 @@ func main() {
 		lentry.Fatal("problem opening %s %v", dbFileNameTransfer, err)
 	}
 
-	accountStorage := store_account.NewStoredAccount(dbAccount)
-	accountToken := store_token.NewStoredToked(dbToken)
-	accountTransfer := store_transfer.NewStoredTransfer(dbTransfer)
-	accountUseCase := account.UseCase{StoredAccount: accountStorage}
-	loginUseCase := authentication.UseCase{AccountUseCase: &accountUseCase, StoredToken: accountToken}
-	transferUseCase := transfer.UseCase{AccountUseCase: &accountUseCase, StoredTransfer: accountTransfer, TokenUseCase: &loginUseCase}
-	accountHandler := http_account.NewHandler(&accountUseCase, lentry)
-	loginHandler := http_login.NewHandler(&loginUseCase, lentry)
-	transferHandler := http_transfer.NewHandler(&transferUseCase, lentry)
-	server := https.NewAPI(accountHandler, loginHandler, transferHandler, lentry)
-	lentry.WithField("Port", Port).Info("starting the server!")
-	if err := http.ListenAndServe(":5000", server); err != nil {
-		lentry.Fatal("could not hear on port 5000 ")
-	}
-	lentry.WithField("Port", Port).Info("shutting down the server")
+	return dbAccount, dbToken, dbTransfer
 }

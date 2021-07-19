68e321d224c3b81dd2d46bcf60bada96f4955a04
rafael <rafaelcastromedrado@gmail.com>
2021-07-19 13:51:16 -0300

    refactor: fix the account

--- storage/postgre/accounts/save_account.go
+++ storage/postgre/accounts/save_account.go
@@ -3,9 +3,12 @@ package accounts
 import "context"
 
 func (a *Storage) SaveAccount(account Account) error {
-	statement := `INSERT INTO accounts(id, name, cpf, secret, balance, created_at)
+	statement := `INSERT INTO accounts(id_account, name, cpf, secret, balance, created_at)
 				  VALUES ($1, $2, $3, $4, $5, $6)`
-	_, err := a.pool.Exec(context.Background(), statement, account.ID, account.Name, account.CPF, account.Secret, account.Balance, account.CreatedAt)
+	comand, err := a.pool.Exec(context.Background(), statement, account.ID, account.Name, account.CPF, account.Secret, account.Balance, account.CreatedAt)
+	if comand.RowsAffected() > 0 {
+		return nil
+	}
 	if err != nil {
 		return err
 	}

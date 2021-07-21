e30d32fabdf406a5df3d81ec894eaadbdeb02465
rafael <rafaelcastromedrado@gmail.com>
2021-07-21 16:45:52 -0300

    refactor: fix the save token with sql

--- storage/postgre/transfer/save_transfer.go
+++ storage/postgre/transfer/save_transfer.go
@@ -3,9 +3,12 @@ package transfer
 import "context"
 
 func (a *Storage) SaveTransfer(transfer Transfer) error {
-	statement := `INSERT INTO accounts(id, origin_account_id, destination_account_id, amount, created_at)
+	statement := `INSERT INTO transfers(id, origin_account_id, destination_account_id, amount, created_at)
 				  VALUES ($1, $2, $3, $4, $5)`
-	_, err := a.pool.Exec(context.Background(), statement, transfer.ID, transfer.OriginAccountID, transfer.DestinationAccountID, transfer.Amount, transfer.CreatedAt)
+	comand, err := a.pool.Exec(context.Background(), statement, transfer.ID, transfer.OriginAccountID, transfer.DestinationAccountID, transfer.Amount, transfer.CreatedAt)
+	if comand.RowsAffected() > 0 {
+		return nil
+	}
 	if err != nil {
 		return err
 	}

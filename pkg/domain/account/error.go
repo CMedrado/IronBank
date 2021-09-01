633d5fe458af76e83bc8406f29df64017cb34ab1
rafael <rafaelcastromedrado@gmail.com>
2021-09-01 18:18:40 -0300

    refactor: correcting errors

--- pkg/domain/account/error.go
+++ pkg/domain/account/error.go
@@ -3,7 +3,6 @@ package account
 import "errors"
 
 var (
-	ErrAccountExists = errors.New("given cpf is already used")
 	ErrBalanceAbsent = errors.New("given the balance amount is invalid")
 	ErrUpdate        = errors.New("unable to update")
 )
